59cab114050bf7776fb953f725c1fb1b8ef308a9
MatheusLBrum <matheusdelimabrum@gmail.com>
2023-06-08 20:18:01 -0300

    reverting changes

--- src/controllers/userController.mjs
+++ src/controllers/userController.mjs
@@ -1,23 +1,23 @@
 import {userQuery} from '../queries/useQuery.mjs'
 
 export const userController = {
-  getAllUsers(req, res) {
+  async getAllUsers(req, res) {
     try {
-      const users = userQuery.getAllUsers();
+      const users = await userQuery.getAllUsers();
 
-      return res
+      return res.send(200).json(users)
     } catch (error) {
-      return error;
+      res.send(500).json(error)
     }
   },
-  createUser(req, res) {
+  async createUser(req, res) {
     try {
       const userData = req.body;
       const newUser = userQuery.createUser();
     } catch (error) {
     }
   },
-  createAdmInformation(req, res) {
+  async createAdmInformation(req, res) {
     try {
       const AdmData = req.body;
       const newAdm = userQuery.createAdmInformations();
